49bc435456c706e26f4c1487bfcfc2eecae28fdc
MoonBready <113399839+MoonBready@users.noreply.github.com>
2023-10-05 21:46:49 -0400

    attack and animation
    currently there's a bug where the player keeps attacking unless left click is pressed again for some reason. idk how to fix it but it's most likely a problem with the input system.

--- TD-TDRPG/Assets/Scripts/PlayerController.cs
+++ TD-TDRPG/Assets/Scripts/PlayerController.cs
@@ -8,39 +8,79 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 1f;
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
+    bool canMove = true;
+    public Attack attack;
 
     Vector2 movementInput;
+
+    SpriteRenderer spriteRenderer;
+
     Rigidbody2D rb;
+
+    Animator animator;
+
     readonly List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate()
     {
-        if (movementInput != Vector2.zero)
+        if (canMove)
         {
-            bool success = TryMove(new Vector2(movementInput.x, 0));
-
-            if (!success)
+            if (movementInput != Vector2.zero)
             {
-                success = TryMove(new Vector2(0, movementInput.y));
+                bool success = TryMove(new Vector2(movementInput.x, 0));
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+
+                if (!success)
+                {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+
+                animator.SetBool("isMoving", success);
+            }
+            else
+            {
+                animator.SetBool("isMoving", false);
+            }
+
+            if (movementInput.x < 0)
+            {
+                spriteRenderer.flipX = true;
+            }
+            else if (movementInput.x != 0)
+            {
+                spriteRenderer.flipX = false;
             }
         }
     }
 
     private bool TryMove(Vector2 direction)
     {
-        int count = rb.Cast
-    (movementInput, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
-
-        if (count == 0)
+        if(direction != Vector2.zero)
         {
-            rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
-            return true;
+            int count = rb.Cast
+                (movementInput, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+            if (count == 0)
+            {
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
         else
         {
@@ -51,6 +91,36 @@ public class PlayerController : MonoBehaviour
     void OnMove(InputValue movementValue)
     {
         movementInput = movementValue.Get<Vector2>();
-        Debug.Log("move");
+        canMove = true;
+    }
+
+    void OnFire()
+    {
+        animator.SetTrigger("Attack");
+        canMove = false;
+    }
+
+    public void Attack()
+    {
+        LockMovement();
+
+        if(spriteRenderer.flipX == true)
+        {
+            attack.AttackLeft();
+        }
+        else
+        {
+            attack.AttackRight();
+        }
+    }
+
+    public void LockMovement()
+    {
+        canMove = false;
+    }
+
+    public void UnlockMovement()
+    {
+        canMove = true;
     }
 }
