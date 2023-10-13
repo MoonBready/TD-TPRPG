d56a06f5b30003975ee2bc45da60fabb79ec44b8
TheGuyYouKnow27 <iannuccills@gmail.com>
2023-10-13 17:20:06 -0400

    slime movements

--- TD-TDRPG/Assets/Scripts/Enemy.cs
+++ TD-TDRPG/Assets/Scripts/Enemy.cs
@@ -1,10 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     Animator animator;
+
+    CapsuleCollider2D capsuleCollider;
+
+    Rigidbody2D rb;
+
+    public float moveSpeed;
+    public Player player;
+    private Vector3 directionToPlayer;
+    private Vector3 localScale;
+    
 
     public float Health
     {
@@ -28,6 +41,10 @@ public class Enemy : MonoBehaviour
     public void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        player = FindObjectOfType(typeof(Player)) as Player;
+        moveSpeed = 3f;
+        localScale = transform.localScale;
     }
 
     public void Defeated()
@@ -38,5 +55,38 @@ public class Enemy : MonoBehaviour
     public void RemoveEnemy()
     {
         Destroy(gameObject);
+    }
+
+    /*public void HitPlayer()
+    {
+        OnCollisionEnter2D && GetComponent<Collider>
+    }*/
+
+    public void FixedUpdate() 
+    {
+        MoveEnemy();
+    }
+
+    public void LateUpdate()
+    {
+        if (rb.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(localScale.x, localScale.y, localScale.z);
+        }
+        else if (rb.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-localScale.x, localScale.y, localScale.z);
+        }    
+    }
+
+    public void MoveEnemy()
+    {
+        directionToPlayer = (player.transform.position - transform.position).normalized;
+        rb.velocity = new Vector2(directionToPlayer.x, directionToPlayer.y) * moveSpeed;
+    }
+
+    public void Update()
+    {
+        
     }
 }
